4ca244f4274bfbff61f6bdb478fa961ea7b4b620
Joseph (Mac) Burton <57452391+limemonkeys@users.noreply.github.com>
2022-01-08 16:53:21 -0400

    Removed token from bot

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-client.run("OTI4MTQ2MzUwNDMwODg4MDM1.YdUhpg.tCAXyzBINqs10jHQFXanj8BkTnM"))
+client.run("BOT_TOKEN"))
