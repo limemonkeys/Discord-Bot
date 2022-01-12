574a7d031f719ca723b41b652931aeeee982b07d
Joseph Burton <js562338@dal.ca>
2022-01-12 11:19:16 -0400

    Added disconnect on inactive

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-#Combined music.py and main.py
+# Base Code Citation: https://www.youtube.com/watch?v=jHZlvRr9KxM
+import discord
+from discord.ext import commands
+import youtube_dl
+import urllib.request
+import re
+import asyncio
+
 
 queue = []
 cog = []
-current_song = None;
+current_song = None
+beginning_check = False
+bot_is_inactive = False
 
 client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
@@ -16,9 +25,11 @@ class music(commands.Cog):
     # TODO: Add asyncio or await to check if playing every 15 seconds
     # Add either here OR in join. I'd recommend join.
     print("Bot online")
+    
 
   @commands.command()
   async def join(self, ctx):
+    global beginning_check
     # If command is being called by user not in a voice channel.
     if ctx.author.voice is None:
       await ctx.send("You cannot issue voice commands outside of a voice channel.")
@@ -29,6 +40,8 @@ class music(commands.Cog):
     # Otherwise, move to voice channel command is called
     else:
       await ctx.voice_client.move_to(voice_channel)
+    beginning_check = True
+    await check_bot_activity(ctx, 15)
 
   @commands.command()
   async def disconnect(self, ctx):
@@ -43,7 +56,70 @@ class music(commands.Cog):
 
   @commands.command()
   async def play(self, ctx, *search_keyword):
-    global current_song, queue
+    global bot_is_inactive
+    global current_song, queue, beginning_check
+    search_keyword_stitched = "+".join(search_keyword)
+
+# Base Code Citation: https://www.youtube.com/watch?v=jHZlvRr9KxM
+import discord
+from discord.ext import commands
+import youtube_dl
+import urllib.request
+import re
+import asyncio
+
+
+queue = []
+cog = []
+current_song = None
+beginning_check = False
+bot_is_inactive = False
+
+client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+
+class music(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+    #self.loop = asyncio.new_event_loop()
+
+  @client.event
+  async def on_ready():
+    # TODO: Add asyncio or await to check if playing every 15 seconds
+    # Add either here OR in join. I'd recommend join.
+    print("Bot online")
+    
+
+  @commands.command()
+  async def join(self, ctx):
+    global beginning_check
+    # If command is being called by user not in a voice channel.
+    if ctx.author.voice is None:
+      await ctx.send("You cannot issue voice commands outside of a voice channel.")
+    voice_channel = ctx.author.voice.channel
+    # If bot is not already in a channel
+    if ctx.voice_client is None:
+      await voice_channel.connect()
+    # Otherwise, move to voice channel command is called
+    else:
+      await ctx.voice_client.move_to(voice_channel)
+    beginning_check = True
+    await check_bot_activity(ctx, 15)
+
+  @commands.command()
+  async def disconnect(self, ctx):
+    #await ctx.voice_client.stop()
+    #await ctx.voice_client.disconnect()
+    
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+      await voice_channel.connect()
+    await ctx.send("> Bye!")
+    await ctx.voice_client.disconnect()
+
+  @commands.command()
+  async def play(self, ctx, *search_keyword):
+    global bot_is_inactive
+    global current_song, queue, beginning_check
     search_keyword_stitched = "+".join(search_keyword)
 
     voice_channel = ctx.author.voice.channel
@@ -62,23 +138,31 @@ class music(commands.Cog):
       info = ydl.extract_info(search_keyword_stitched, download = False)
       current_song = search_keyword_stitched
       # current_song is the URL
-      if not ctx.voice_client.is_playing():
-        try:
-          vc = ctx.voice_client
-          url2 = info['formats'][0]['url']
-          '''
-          loop = asyncio.get_event_loop()
-          loop.create_task(play_next(ctx))
-          vc.play(discord.FFmpegPCMAudio(source=url2), after=lambda e: loop) 
-          '''
-          vc.play(discord.FFmpegPCMAudio(source=url2), after=lambda e: play_next(ctx))
-          print("Done queue")
-        except:
+      try:
+        if not ctx.voice_client.is_playing():
+          bot_is_inactive = False;
+          try:
+            vc = ctx.voice_client
+            url2 = info['formats'][0]['url']
+            '''
+            loop = asyncio.get_event_loop()
+            loop.create_task(play_next(ctx))
+            vc.play(discord.FFmpegPCMAudio(source=url2), after=lambda e: loop) 
+            '''
+            vc.play(discord.FFmpegPCMAudio(source=url2), after=lambda e: play_next(ctx))
+            print("Done queue")
+          except:
+            queue.append(search_keyword_stitched)
+            print("Audio not playing, but in process")
+            return
+        else:
           queue.append(search_keyword_stitched)
-          print("Audio not playing, but in process")
           return
-      else:
-        queue.append(search_keyword_stitched)
+        if not beginning_check:
+          beginning_check = True
+          await check_bot_activity(ctx, 15)
+      except:
+        print("Issues with ctx.voice_client.is_playing()")
         return
     
     
@@ -135,7 +219,27 @@ class music(commands.Cog):
 
   
 
-      
+async def check_bot_activity(ctx, seconds):
+  global check_for_activity, bot_is_inactive
+  await asyncio.sleep(seconds)
+  print("Check bot hehe")
+  try:
+    if (ctx.voice_client.is_playing() or len(queue) > 0):
+      bot_is_inactive = False
+      await check_bot_activity(ctx, seconds)
+    elif not bot_is_inactive:
+      print("Bot isn't playing. Leaving soon.")
+      bot_is_inactive = True
+      await check_bot_activity(ctx, seconds)
+    else:
+      print("Bot leaving")
+      await ctx.send("> I'm inactive. Cya!")
+      await ctx.voice_client.disconnect()
+  except:
+    print("Issues with ctx.voice_client.is_playing()")
+    return
+  
+
     
 
 def setup(client):
@@ -181,3 +285,6 @@ def play_next(ctx):
 
 cog.append(setup(client))
 client.run("BOT TOKEN")
+
+    
+        
